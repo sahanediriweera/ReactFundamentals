2845c5e35f78444203c01ca3b0977d184cbe83c5
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-06 12:48:11 +0530

    completed css parts

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,8 +1,39 @@
 import React from 'react'
 
+// const Header = () => {
+//   return (
+//     <header style={{
+//       backgroundColor: 'royalblue',
+//       color:'#fff'
+//     }}>
+//         <h1>
+//             Groceries List
+//         </h1>
+//     </header>
+//   )
+// }
+
+
+// const Header = () => {
+//   const headerStyles = {
+//     backgroundColor:'royalblue',
+//     color:'red'
+//   };
+
+//   return (
+//     <header style={headerStyles}>
+//         <h1>
+//             Groceries List
+//         </h1>
+//     </header>
+//   )
+// }
 const Header = () => {
   return (
-    <header>
+    <header style={{
+      backgroundColor: 'royalblue',
+      color:'#fff'
+    }}>
         <h1>
             Groceries List
         </h1>
