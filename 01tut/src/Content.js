c1b74aa2fe1e601c75db5ed9dbc31a92297a576a
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-06 13:25:51 +0530

    completed useStates

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useState } from 'react';
 
-const handleNameChange = () =>{
-    const names = ['bob','mil','lucy'];
-    const int = Math.floor(Math.random()*3);
-    return names[int];
-};
+
 
 const Content = () => {
+  const [name,setName] = useState('Sahan');
+  const [count,setCount] = useState(0);
+
+  const handleNameChange = () =>{
+    const names = ['bob','mil','lucy'];
+    const int = Math.floor(Math.random()*3);
+    setName(names[int]);
+  };
   const handleClick = () => {
     console.log("The button was clicked");
   };
@@ -18,10 +23,19 @@ const Content = () => {
   const handleClickEvent = (e)=> {
     console.log(e);
   }
+
+  const handleCount = () => {
+    setCount(count+1);
+    console.log(count);
+  };
+
+  const handleCountCurrentValue = () => {
+    console.log(count);
+  }
   
   return (
     <main>
-        Hellow {handleNameChange()}!
+        Hellow {name}!
 
         <button onClick={handleClick}>
           Click It
@@ -34,6 +48,19 @@ const Content = () => {
         <button onClick={(e)=>{handleClickEvent(e)}}>
           Click for event 
         </button>
+
+        <button onClick={handleNameChange}>
+          Click to use State 1
+        </button>
+
+        <button onClick={handleCount}>
+          Click to change count 
+        </button>
+
+        <button onClick={handleCountCurrentValue}>
+          Click to view Count 
+        </button>
+
     </main>
   )
 }
