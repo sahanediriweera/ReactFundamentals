4486c23307468f9d317b53f1f241c8255cf3cd9e
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-15 15:39:11 +0530

    completed project

---
+++ 02tut/src/Square.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Square = ({colorValue,hexValue,IsDarkText}) => {
+  return (
+    <section
+        className='square'
+        style={{
+          backgroundColor:colorValue,
+          color:IsDarkText? "#000":"#FFF"
+        }}
+    >
+        <p>
+            {colorValue? colorValue: "Empty Value"}
+        </p>
+        <p>
+            {hexValue? hexValue: null}
+        </p>
+    </section>
+  )
+}
+
+Square.defaultProps = {
+    colorValue:"Empty Color Value"
+};
+
+export default Square
