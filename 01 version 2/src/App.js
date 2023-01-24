5d1b04f05e446a041e134f7559c6deebfbf976f9
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-24 22:34:16 +0530

    CRUD Operations

--- 01 version 2/src/App.js
+++ 01 version 2/src/App.js
@@ -4,6 +4,7 @@ import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState,useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3000/items';
@@ -40,16 +41,42 @@ function App() {
     setItems(newItems);
   }
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setAndSaveItems(listItems);
+
+    const postOptions = {
+      method:POST,
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+
+    const result = await apiRequest(API_URL,postOptions);
+    if(result) setFetError(result);
+
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setAndSaveItems(listItems);
+
+    const myItem = listItems.filter((item)=> item.id === id);
+    const updateOptions = {
+      method:'PATCH',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({checked:myItem[0].checked})
+    }
+
+    const reqUrl = `${API_URL}/${id}`;
+
+    const result = await apiRequest(reqUrl,updateOptions);
+    if(result) setFetError(result);
   }
 
   const handleDelete = (id) => {
