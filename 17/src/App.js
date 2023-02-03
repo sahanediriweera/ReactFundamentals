eda7c2c683c5a26ba1c04792573247618b2e2702
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-03 22:41:08 +0530

    completed axios

--- 17/src/App.js
+++ 17/src/App.js
@@ -8,7 +8,10 @@ import About from './About';
 import Missing from './Missing';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { format } from 'date-fns';
+import { format, set } from 'date-fns';
+import api from './api/posts';
+import EditPosts from './EditPosts';
+import useWindowSize from './Hooks/useWindowSize';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -41,7 +44,28 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle,setEditTitle] = useState('');
+  const [editBody,setEditBody] = useState('');
   const history = useHistory();
+  const {width} = useWindowSize();
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      try{
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      }catch (err){
+          if(err.response){
+          console.log(err.response.data.message);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+          } else{
+            console.log(err.message);
+          }
+      }
+    }
+    fetchPosts();
+  },[])
 
   useEffect(() => {
     const filteredResults = posts.filter((post) =>
@@ -51,27 +75,51 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    history.push('/');
+    try{
+      const response = await api.post('/posts',newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      history.push('/');
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    history.push('/');
+  const handleEdit = async (id)=> {
+    const datetime = format(new Date(), 'MMMM dd,yyyy pp');
+    const updatedPost = { id, title: postTitle, datetime, body: postBody };
+    try{
+      const response = await api.put(`/posts/${id}`,updatedPost);
+      setPosts(posts.map(post => post.id === id ? {...response.data}:post));
+      setEditTitle('');
+      setEditBody('');
+      history.push('/');
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try{
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      history.push('/');
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 
   return (
     <div className="App">
-      <Header title="React JS Blog" />
+      <Header title="React JS Blog" width = {width}/>
       <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
@@ -84,6 +132,16 @@ function App() {
             setPostTitle={setPostTitle}
             postBody={postBody}
             setPostBody={setPostBody}
+          />
+        </Route>
+        <Route exact path="/edit/:id">
+          <EditPosts
+            posts={posts}
+            handleEdit = {handleEdit}
+            editTitle = {editTitle}
+            setEdittitle = {setEditTitle}
+            editBody = {editBody}
+            setEditBody = {setEditBody}
           />
         </Route>
         <Route path="/post/:id">
---
+++ 17/src/Hooks/useWindowSize.js
@@ -0,0 +1,36 @@
+import { useState,useEffect } from "react";
+
+const useWindowSize = ()=> {
+    const [windowSize,setWindowSize] = useState({
+        width:undefined,
+        height:undefined
+    });
+
+
+    useEffect(()=> {
+        const handleResize = ()=> {
+            setWindowSize({
+                width:window.innerWidth,
+                height:window.innerHeight
+            });
+        }
+
+        handleResize();
+
+        window.addEventListener("resize",handleResize);
+
+        const cleanUp = ()=> {
+            console.log('runs when an useEffect dependency changes');
+            window.removeEventListener("resize",handleResize);
+        }
+
+        return cleanUp;
+
+    },[])
+
+    return windowSize;
+
+
+}
+
+export default useWindowSize;
