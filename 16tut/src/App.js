0648a15e89aa7ad9cd6dd4be4cadcb1fcd803c6d
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-03 14:23:23 +0530

    completed react routes

--- 16tut/src/App.js
+++ 16tut/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import Missing from './Missing';
+import Home from "./Home";
+import {Route, Routes, useHistory} from 'react-router-dom';
+import { useState,useEffect } from "react";
+import About from "./About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Nav/>
+        <Routes>
+          <Route path="/" element= {<Home/>}/>
+          <Route path="/post" element = {<NewPost/>}/>
+          <Route path="/post/:id" element = {<PostPage/>}/>
+          <Route path="/about" element = {<About/>}/>
+          <Route path="*" element = {<Missing/>}/>
+        </Routes>
+        <Footer/>
     </div>
   );
 }
